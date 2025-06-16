6817f3d9bc1871270dfcddd3d8b28418fcba914c
sarter <armanospanov212@gmail.com>
2025-06-17 02:23:49 +0500

    Первый коммит  сайт для неё

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+  const message = document.getElementById('hidden-message');
+  message.style.display = 'block';
+  message.style.animation = 'fadeInText 1s ease-in-out';
+}
