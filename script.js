ae7b1615e352b7ebae9e1130a1c7d02680e87587
sarter <armanospanov212@gmail.com>
2025-06-17 02:47:04 +0500

    Обновил сайт: добавил печатающийся текст и переключение темы

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function showMessage() {
   message.style.display = 'block';
   message.style.animation = 'fadeInText 1s ease-in-out';
 }
+
+function showMessage() {
+  document.getElementById("hidden-message").style.display = "block";
+}
+
+document.getElementById('theme-toggle').addEventListener('click', function () {
+  document.body.classList.toggle('dark');
+});
